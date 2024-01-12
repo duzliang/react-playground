81b5db30c819e157dbeef49755786160aa98b828
杜志良 <duzhiliang@feewee.cn>
2024-01-12 14:07:53 +0800

    test context

---
+++ src/context/ImageList/App.jsx
@@ -0,0 +1,62 @@
+import { useContext, useState } from 'react';
+import { places } from './data.js';
+import { getImageUrl } from './utils.js';
+import { ImageContext } from './Context';
+
+export default function App() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+  return (
+    <>
+      <ImageContext.Provider value={imageSize}>
+        <label>
+          <input
+            type="checkbox"
+            checked={isLarge}
+            onChange={e => {
+              setIsLarge(e.target.checked);
+            }}
+          />
+          Use large images
+        </label>
+        <hr />
+        <List />
+      </ImageContext.Provider>
+    </>
+  );
+}
+
+function List() {
+  const listItems = places.map(place =>
+    <li key={place.id}>
+      <Place
+        place={place}
+      />
+    </li>,
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function Place({ place }) {
+  return (
+    <>
+      <PlaceImage place={place} />
+      <p>
+        <b>{place.name}</b>
+        {': ' + place.description}
+      </p>
+    </>
+  );
+}
+
+function PlaceImage({ place }) {
+  const imageSize = useContext(ImageContext);
+  return (
+    <img
+      src={getImageUrl(place)}
+      alt={place.name}
+      width={imageSize}
+      height={imageSize}
+    />
+  );
+}
